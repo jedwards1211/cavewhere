16a7b1aec08847a37edf048af7e2e566b0738eff
Andy Edwards <jedwards@fastmail.com>
2015-08-18 23:51:54 -0500

    update cwWallsImporter to comply with station-renaming-changes merge

--- src/cwWallsImporter.cpp
+++ src/cwWallsImporter.cpp
@@ -270,7 +270,6 @@ void cwWallsImporter::runTask()
     Caves.append(cwCave());
     cwCave* cave = &Caves.last();
     cave->setName("Walls Import");
-    StationRenamer.setCave(cave);
 
     QList<cwTrip*> trips;
 
